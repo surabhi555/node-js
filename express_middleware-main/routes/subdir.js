92ea875ca0289aff6ce24c6cb13af65b6e37c65b
surabhi555 <pednekarsurabhi555@gmail.com>
2025-01-13 12:07:27 +0530

    Rest Api

---
+++ express_middleware-main/routes/subdir.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const router =express.Router()
+
+const path = require('path')
+
+router.get('^/$|/index(.html)?', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, '..' ,'views','subdir', 'index.html'));
+});
+router.get('^/$|/index(.html)?', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, '..' ,'views','subdir', 'index.html'));
+});
+router.get('^/$|/index(.html)?', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, '..' ,'views','subdir', 'index.html'));
+});
+
+module.exports = router;
